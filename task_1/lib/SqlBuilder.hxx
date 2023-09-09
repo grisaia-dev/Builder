3eb0be03e88feb4b26986d07ab207d8261b7d741
JateX <grisaia@inbox.ru>
2023-09-09 12:42:40 +0300

    Initial commit

---
+++ task_1/lib/SqlBuilder.hxx
@@ -0,0 +1,6 @@
+#pragma once
+
+class SqlSelectQueryBuilder {
+public:
+private:
+};
---
+++ task_1/main.cxx
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "lib/SqlBuilder.hxx"
+
+int main(void) {
+	std::cout << "Hello World!!" << std::endl;
+	return EXIT_SUCCESS;
+}
